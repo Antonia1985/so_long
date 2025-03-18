543d29a6cbf8bff4b0b57f15a965f7328945ef51
Antonia <33874536+Antonia1985@users.noreply.github.com>
2025-03-18 19:30:17 +0100

    Create ft_atoi.c

---
+++ libft/include/src/ft_atoi.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavlopo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 17:20:49 by apavlopo          #+#    #+#             */
+/*   Updated: 2024/11/09 17:21:33 by apavlopo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	size_t	i;
+	char	*space_char;
+
+	i = 0;
+	space_char = " \t\n\v\f\r";
+	while (i < 6)
+	{
+		if (c == space_char[i])
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		signd;
+	int		sign;
+	int		num;
+
+	i = 0;
+	signd = 0;
+	sign = 1;
+	num = 0;
+	while (ft_isspace(nptr[i]) == 1)
+		i++;
+	if (signd == 0 && (nptr[i] == '+' || nptr[i] == '-'))
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		signd = 1;
+		i++;
+	}
+	if ((signd == 1) && (nptr[i] == '+' || nptr[i] == '-'))
+		return (num);
+	while ((nptr[i] != '\0') && (nptr[i] >= 48) && (nptr[i] <= 57))
+		num = (num * 10) + (nptr[i++] - '0');
+	num = num * sign;
+	return (num);
+}
